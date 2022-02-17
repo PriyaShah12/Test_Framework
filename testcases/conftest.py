ab565be0bac40cc0599e6aa5a3322e7656230b83
PriyaShah12 <prshah160@gmail.com>
2022-02-17 17:42:25 -0500

    revised code

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -8,7 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.driver import OperaDriver
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager, IEDriverManager
-
 
 @pytest.fixture(scope="class")
 def setup(request, browser):
@@ -40,25 +37,14 @@ def pytest_addoption(parser):
 def browser(request):
     return request.config.getoption("--browser")
 
-@pytest.mark.usefixtures("setup")
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
-    pytest_html = item.config.pluginmanager.getplugin("html")
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, "extra", [])
-    if report.when == "call":
-        # always add url to report
-        extra.append(pytest_html.extras.url("http://www.yatra.com/"))
-        xfail = hasattr(report, "wasxfail")
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            report_directory= os.path.dirname(item.config.option.htmlpath)
-            file_name=report.nodeid.replace("::","_")+ ".png"
-            destinationFile= os.path.join(report_directory, file_name)
-            driver.save_screenshot(destinationFile)
-            # only add additional html on failure
-            extra.append(pytest_html.extras.html("<div>Additional HTML</div>"))
-        report.extra = extra
+def pytest_configure(config):
+    config._metadata["Project Name"] = "Airlines-Yatra"
+    config._metadata["Tester"] = "Priya"
+
+@pytest.mark.optionalhook
+def pytest_configure(config):
+    config._metadata.pop("JAVA_HOME", None)
+    config._metadata.pop("Plugins", None)
 
 def pytest_html_report_title(report):
     report.title= "Priya Shah Automation Report"
